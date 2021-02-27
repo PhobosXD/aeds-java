77da4dc1e20ad325e6f3d5cce4519be72ef9a638
Igor Emerick <igoremerickbarbosa@hotmail.com>
2021-02-27 15:51:26 -0300

    Reiniciando o projeto

--- src/main/java/phobosxd/Main.java
+++ src/main/java/phobosxd/Main.java
@@ -1,8 +1,5 @@
 package phobosxd;
 
-import phobosxd.filaestatica.FilaEstatica;
-import phobosxd.listaestatica.ListaEstatica;
-
 /**
  * @author Cazuza
  * @since 2021-02-23
@@ -15,23 +12,6 @@ public class Main {
      * @param args Argumentos da main.
      */
     public static void main(String[] args) {
-        final var lista = new ListaEstatica();
 
-        for (int i = 0; i < 10; i++) {
-            if (!lista.push_back(i)) {
-                System.out.println("Nao foi possivel inserir na fila!");
-            }
-        }
-
-        lista.pop_front();
-        lista.pop_front();
-        lista.push(0, 1);
-        lista.push_front(1);
-        lista.pop_back();
-        lista.pop(3);
-
-        for (Integer i = 0; i < lista.getTamanho(); i++) {
-            System.out.println(lista.getElement(i));
-        }
     }
 }
