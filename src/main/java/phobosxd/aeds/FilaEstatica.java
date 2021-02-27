bc4d7b2a7dd8b0cbb14efa3cfea0fdd8baf927f4
Igor Emerick <igoremerickbarbosa@hotmail.com>
2021-02-27 19:39:30 -0300

    Alterações de javadoc

--- src/main/java/phobosxd/aeds/FilaEstatica.java
+++ src/main/java/phobosxd/aeds/FilaEstatica.java
@@ -1,6 +1,18 @@
 package phobosxd.aeds;
 
+/**
+ * @author PhobosXD
+ * @since 2021-02-27
+ * @version 3.0
+ */
 public class FilaEstatica extends EstruturaEstatica {
+    /**
+     * Retorna o elemento da posição desejada.
+     *
+     * @param position Integer: Posição do elemento.
+     * @return Integer: Elemento, quando a posição é válida, ou null, quando a
+     * posição não é válida.
+     */
     public Integer getElement(final Integer position) {
         if (position >= 0 && position < size) {
             Integer pos = position + primeiro;
@@ -10,7 +22,7 @@ public class FilaEstatica extends EstruturaEstatica {
 
             return vector[pos];
         }
-        
+
         return null;
     }
 }
